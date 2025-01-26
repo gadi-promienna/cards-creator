903c0d3e6219c1db5e17a0d40fae7e3d20efa224
gadi-promienna <antrbarbara@webdeveloper@gmail.com>
2025-01-26 14:04:38 +0100

    Add new list form and sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,37 @@
+import { List, ListContextType } from "../@types/list";
+import useListsContext from "../hooks/use-lists-context";
+import { useEffect } from "react";
+
+function Sidebar() {
+    const {fetchLists, lists, list} = useListsContext() as ListContextType;
+    
+    useEffect(() => {
+        fetchLists()
+        },[fetchLists])
+
+    const renderedLists = lists.map( 
+        (list)=>{ 
+            const renderedWords = list.words.map((word)=>
+                {
+                    return(<li>{word}</li>)
+                }
+            )
+            return( 
+                <li>
+                    <h3>{list.name}</h3>
+                    <ul>
+                        {renderedWords}
+                    </ul>
+                </li>
+             )
+        }
+    )
+    return(
+        <div className="sidebar">
+            <h2>Twoje Listy</h2>
+            {renderedLists}
+        </div>
+    )
+}
+
+export default Sidebar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,10 @@
-import { object } from "prop-types";
-import { List, ListContextType } from "../@types/list";
-import { useEffect } from "react";
-import useListsContext from "../hooks/use-lists-context";
 import NewListForm from "../components/NewListForm";
+import Sidebar from "../components/Sidebar";
 function Home(){
-    const {fetchLists, lists, list} = useListsContext() as ListContextType;
-    
-    useEffect(() => {
-        fetchLists()
-    },
-    [fetchLists]
-    )
-
-    const renderedLists = lists.map( 
-        (list)=>{ 
-            return( 
-                <>
-                    <h3>{list.name}</h3>
-                    <p className="text">{list.words}</p>
-                </>
-             )
-        }
-    )
     return(
         <div>
-             {/* <NewListForm></NewListForm> */}
-            <h2>Twoje Listy</h2>
-            {renderedLists}
+             <NewListForm></NewListForm>
+             <Sidebar></Sidebar>
         </div>
     )
 }
